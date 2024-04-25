32bdbd7be02460b1034e7262107b2ed27faeec85
BrunoDXD <146298531+BrunoDXD@users.noreply.github.com>
2024-04-24 22:10:17 -0300

    Add files via upload

---
+++ Negocios/Cliente.cs
@@ -0,0 +1,64 @@
+﻿using AcessoBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class Cliente
+    {
+        public string cpf { get; set; }
+        public string nome { get; set; }
+        public string endereco { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        Conexao con;
+
+        public bool Salvar()
+        {
+            con = new Conexao();
+            string insere = $"insert into tb04_cliente values (" +
+                $"'{cpf}', '{nome}', '{endereco}', '{telefone}', '{email}')";
+
+            return con.Executa(insere);
+
+        }
+
+        public bool Atualizar()
+        {
+            con = new Conexao();
+            string atualiza = $"update tb04_cliente set tb04_nome= '{nome}', " +
+                $"tb03_endereco='{endereco}', tb03_telefone='{telefone}', tb03_email='{email}'," +
+                $" where tb03_cpf={cpf}";
+            return con.Executa(atualiza);
+
+        }
+
+        public bool Excluir()
+        {
+            con = new Conexao();
+            string excluir = $"delete from tb04_cliente where tb04_cpf={cpf}";
+            return con.Executa(excluir);
+
+        }
+
+        public DataTable Pesquisar()
+        {
+            con = new Conexao();
+            string pesquisa = $"select * from tb04_cliente where tb04_cpf='{cpf}'";
+            return con.Retorna(pesquisa);
+        }
+
+        public DataTable PesquisarTodos()
+        {
+            con = new Conexao();
+            string pesquisa = $"select * from tb04_cliente";
+            return con.Retorna(pesquisa);
+        }
+    }
+
+   
+}
---
+++ Negocios/Fornecedor.cs
@@ -0,0 +1,62 @@
+﻿using AcessoBD;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocios
+{
+    public class Fornecedor
+    {
+        public int Id { get; set; }
+        public string Razao { get; set; }
+        public string Cnpj { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        Conexao con;
+
+        public bool Salvar()
+        {
+            con = new Conexao();
+            string insere = $"insert into tb03_fornecedor values (" +
+                $"null, '{Razao}', '{Cnpj}', '{Telefone}', '{Email}')";
+
+            return con.Executa(insere);
+
+        }
+
+        public bool Atualizar()
+        {
+            con = new Conexao();
+            string atualiza = $"update tb03_fornecedor set tb03_razao= '{Razao}', " +
+                $"tb03_cnpj='{Cnpj}', tb03_telefone='{Telefone}', tb03_email='{Email}'," +
+                $" where tb03_id={Id}";
+            return con.Executa(atualiza);
+
+        }
+
+        public bool Excluir()
+        {
+            con = new Conexao();
+            string excluir = $"delete from tb03_fornecedor where tb03_id={Id}";
+            return con.Executa(excluir);
+
+        }
+
+        public DataTable Pesquisar()
+        {
+            con = new Conexao();
+            string pesquisa = $"select * from tb03_fornecedor where tb03_cnpj='{Cnpj}'";
+            return con.Retorna(pesquisa);
+        }
+
+        public DataTable PesquisarTodos()
+        {
+            con = new Conexao();
+            string pesquisa = $"select * from tb03_fornecedor";
+            return con.Retorna(pesquisa);
+        }
+    }
+}
